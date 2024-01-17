ab210f209a262dc2cb193304be3f65def6a86ac6
deerak-isaack <oderaisaack@gmail.com>
2024-01-17 20:22:47 +0300

    model deployment

--- Choosing a Dataset/time-series/save_model.py
+++ Choosing a Dataset/time-series/save_model.py
@@ -1,15 +1,21 @@
 import joblib 
+import os    
 import streamlit as st 
 
+import os
 
-model = joblib.load(open('house_predictor.pkl', 'rb')) 
+script_directory = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(script_directory, 'house_predictor.pkl')
+model = joblib.load(open(model_path, 'rb'))
 
-def main(title="House Predictor".upper()):   
+# model = joblib.load(open('C:/Users/user/Desktop/Phase_IV/dsc-phase4-project/Choosing a Dataset/time-series/house_predictor.pkl', 'rb'))
+
+def main():   
     
     st.title("House Predictor".upper())
     # st.markdown(f'<h1 style="color:#ff0000;">{title}</h1>', unsafe_allow_html=True)
     
-    st.image('house.jpg', use_column_width=True) 
+    # st.image('house.jpg', use_column_width=True) 
     
     info = ''
     
